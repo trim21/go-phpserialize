a4ca467a796e07ffebeb34348880b4dca6100331
Trim21 <trim21.me@gmail.com>
2022-07-07 08:26:18 +0800

    test string

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -66,16 +66,15 @@ func TestUnmarshal_struct_empty(t *testing.T) {
 }
 
 func TestUnmarshal_struct_string(t *testing.T) {
-	t.Skip()
 	t.Parallel()
 
 	type Container struct {
-		F string `php:"f1q,string"`
-		V bool   `php:"1a9,string"`
+		F string `php:"f1q"`
+		V bool   `php:"1a9"`
 	}
 
 	var c Container
-	raw := `a:1:{s:3:"f1q";s:10:"0147852369"}`
+	raw := `a:1:{s:3:"f1q";s:10:"0147852369";}`
 	err := phpserialize.Unmarshal([]byte(raw), &c)
 	require.NoError(t, err)
 	require.Equal(t, "0147852369", c.F)
@@ -122,7 +121,7 @@ func TestUnmarshal_struct_uint(t *testing.T) {
 		raw := `a:1:{s:3:"f1q";i:147852369;}`
 		err := phpserialize.Unmarshal([]byte(raw), &c)
 		require.NoError(t, err)
-		require.EqualValues(t, 147852369, c.F)
+		require.Equal(t, uint(147852369), c.F)
 	})
 
 	t.Run("uint8", func(t *testing.T) {
@@ -134,7 +133,106 @@ func TestUnmarshal_struct_uint(t *testing.T) {
 		raw := `a:1:{s:3:"f1q";i:255;}`
 		err := phpserialize.Unmarshal([]byte(raw), &c)
 		require.NoError(t, err)
-		require.EqualValues(t, 255, c.F)
+		require.Equal(t, uint8(255), c.F)
 	})
 
+	t.Run("uint16", func(t *testing.T) {
+		type Container struct {
+			F uint16 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:574;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, uint16(574), c.F)
+	})
+
+	t.Run("uint32", func(t *testing.T) {
+		type Container struct {
+			F uint32 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:57400;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, uint32(57400), c.F)
+	})
+
+	t.Run("uint64", func(t *testing.T) {
+		type Container struct {
+			F uint64 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:5740000;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, uint64(5740000), c.F)
+	})
+}
+
+func TestUnmarshal_struct_int(t *testing.T) {
+	t.Parallel()
+
+	t.Run("int", func(t *testing.T) {
+		type Container struct {
+			F int `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:147852369;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, int(147852369), c.F)
+	})
+
+	t.Run("int8", func(t *testing.T) {
+		type Container struct {
+			F int8 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:65;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, int8(65), c.F)
+	})
+
+	t.Run("int16", func(t *testing.T) {
+		type Container struct {
+			F int16 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:574;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, int16(574), c.F)
+	})
+
+	t.Run("int32", func(t *testing.T) {
+		type Container struct {
+			F int32 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:57400;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, int32(57400), c.F)
+	})
+
+	t.Run("int64", func(t *testing.T) {
+		type Container struct {
+			F int64 `php:"f1q"`
+		}
+
+		var c Container
+		raw := `a:1:{s:3:"f1q";i:5740000;}`
+		err := phpserialize.Unmarshal([]byte(raw), &c)
+		require.NoError(t, err)
+		require.Equal(t, int64(5740000), c.F)
+	})
 }
