c3585a47c215798ce875e750e735fec58b11245b
Trim21 <trim21.me@gmail.com>
2022-07-09 01:25:06 +0800

    remove dead code

--- internal/encoder/unsafe.go
+++ internal/encoder/unsafe.go
@@ -3,7 +3,6 @@
 package encoder
 
 import (
-	"reflect"
 	"unsafe"
 
 	"github.com/trim21/go-phpserialize/internal/runtime"
@@ -17,15 +16,12 @@ func ptrToUnsafePtr(p uintptr) unsafe.Pointer {
 	return *(*unsafe.Pointer)(unsafe.Pointer(&p))
 }
 
-func reflectValueToLocal(value reflect.Value) rValue {
-	return *(*rValue)(unsafe.Pointer(&value))
-}
-
 type emptyInterface struct {
 	typ *runtime.Type  // value type
 	ptr unsafe.Pointer // value address
 }
 
+// will be used to support interface with methods
 type nonEmptyInterface struct {
 	itab *struct {
 		ityp *runtime.Type // static interface type
@@ -33,39 +29,4 @@ type nonEmptyInterface struct {
 		// unused fields...
 	}
 	ptr unsafe.Pointer
-}
-
-// to get reflect.Value unexported ptr field.
-// used in map encoder
-
-// to get reflect.Value unexported ptr field.
-// used in map encoder
-type rValue struct {
-	typ  *rtype
-	ptr  uintptr
-	flag uintptr
-}
-
-// rtype is the common implementation of most values.
-// It is embedded in other struct types.
-//
-// rtype must be kept in sync with ../runtime/type.go:/^type._type.
-type rtype struct {
-	size       uintptr
-	ptrdata    uintptr // number of bytes in the type that can contain pointers
-	hash       uint32  // hash of type; avoids computation in hash tables
-	tflag      uint8   // extra type information flags
-	align      uint8   // alignment of variable with this type
-	fieldAlign uint8   // alignment of struct field with this type
-	kind       uint8   // enumeration for C
-	// function for comparing objects of this type
-	// (ptr to object A, ptr to object B) -> ==?
-	equal     func(unsafe.Pointer, unsafe.Pointer) bool
-	gcdata    *byte // garbage collection data
-	str       int32 // string form
-	ptrToThis int32 // type for pointer to this type, may be zero
-}
-
-func ifcePtrToValuePtr(p uintptr) unsafe.Pointer {
-	return ((*emptyInterface)(unsafe.Pointer(p))).ptr
 }
