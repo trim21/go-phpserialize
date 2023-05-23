ed3344f072e82d7d93f969378ada7f98bdfb445f
Trim21 <trim21.me@gmail.com>
2023-05-23 16:13:17 +0800

    chore: better newer golang version error message

--- internal/encoder/any.go
+++ internal/encoder/any.go
@@ -1,3 +1,5 @@
 package encoder
 
 type any = interface{}
+
+var _ = NEWER_GO_VERSION_NOT_SUPPORTED_YET
---
+++ internal/encoder/version.go
@@ -0,0 +1,9 @@
+// golang version constraint, only support tested version.
+
+//go:build !go1.21
+
+package encoder
+
+// NEWER_GO_VERSION_NOT_SUPPORTED_YET variable to check tested and supported go version.
+// If you saw this variable is not defined in your go version, please try to update to latest version.
+const NEWER_GO_VERSION_NOT_SUPPORTED_YET = 0
