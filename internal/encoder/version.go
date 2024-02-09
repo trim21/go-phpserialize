cbc71250d0c6c2d0bce7395050b6caa868cebd9d
Trim21 <trim21.me@gmail.com>
2024-02-09 23:07:51 +0800

    chore: support go 1.22 (#47)

--- internal/encoder/version.go
+++ internal/encoder/version.go
@@ -1,9 +1,9 @@
 // golang version constraint, only support tested version.
 
-//go:build !go1.22
+//go:build !go1.23
 
 package encoder
 
-// golang_version_higher_than_1_21_not_supported_yet variable to check tested and supported go version.
+// golang_version_higher_than_1_22_not_supported_yet variable to check tested and supported go version.
 // If you saw this variable is not defined in your go version, please try to update to latest version.
-const golang_version_higher_than_1_21_not_supported_yet = 0
+const golang_version_higher_than_1_22_not_supported_yet = 0
