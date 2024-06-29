838a49acc73494c06f8eccc1313d0764b70bac43
Trim21 <trim21.me@gmail.com>
2024-06-29 22:19:21 +0800

    refactor: add assume-no-moving-gc

--- marshal.go
+++ marshal.go
@@ -1,6 +1,10 @@
 package phpserialize
 
-import "github.com/trim21/go-phpserialize/internal/encoder"
+import (
+	_ "go4.org/unsafe/assume-no-moving-gc"
+
+	"github.com/trim21/go-phpserialize/internal/encoder"
+)
 
 func Marshal(v any) ([]byte, error) {
 	return encoder.Marshal(v)
