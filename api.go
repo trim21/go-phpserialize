905d8591c425fa008be7d3d4d09a7af946b87ddb
Trim21 <trim21.me@gmail.com>
2024-07-15 23:58:01 +0800

    ci: test on go 1.23rc1 (#52)

---
+++ api.go
@@ -0,0 +1,25 @@
+//go:build !go1.24
+
+package phpserialize
+
+import (
+	"fmt"
+
+	"github.com/trim21/go-phpserialize/internal/encoder"
+)
+
+func Marshal(v any) ([]byte, error) {
+	return encoder.Marshal(v)
+}
+
+func Unmarshal(data []byte, v any) error {
+	if len(data) == 0 {
+		return fmt.Errorf("empty bytes")
+	}
+
+	return unmarshal(data, v)
+}
+
+type Unmarshaler interface {
+	UnmarshalPHP([]byte) error
+}
--- internal/encoder/version.go
+++
@@ -1,9 +0,0 @@
-// golang version constraint, only support tested version.
-
-//go:build !go1.23
-
-package encoder
-
-// golang_version_higher_than_1_22_not_supported_yet variable to check tested and supported go version.
-// If you saw this variable is not defined in your go version, please try to update to latest version.
-const golang_version_higher_than_1_22_not_supported_yet = 0
