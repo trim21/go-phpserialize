fc8ed23a660ff43f81f49d4fe97d7de88561be7a
Trim21 <trim21.me@gmail.com>
2024-07-16 01:13:14 +0800

    improve docs

--- api.go
+++ api.go
@@ -5,15 +5,18 @@ package phpserialize
 import (
 	"fmt"
 
-	"github.com/trim21/go-phpserialize/internal/decoder"
 	"github.com/trim21/go-phpserialize/internal/encoder"
 )
 
-// Marshaler allow users to implement its own encoder
+// Marshaler allow users to implement its own encoder.
 // **it's return value will not be validated**, please make sure you return valid encoded bytes.
-type Marshaler = encoder.Marshaler
+type Marshaler interface {
+	MarshalPHP() ([]byte, error)
+}
 
-type Unmarshaler = decoder.Unmarshaler
+type Unmarshaler interface {
+	UnmarshalPHP([]byte) error
+}
 
 func Marshal(v any) ([]byte, error) {
 	return encoder.Marshal(v)
